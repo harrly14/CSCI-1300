6c2cf93a5ffcc7c8cdeb3eac2fcaa8177f3ec084
Sully <50939559+harrly14@users.noreply.github.com>
2024-09-17 15:24:04 -0600

    Add files via upload

---
+++ Week 3 rec/Problem 3.1.c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 7;
+    if (num==0) { //changed from "num" to "num == 0"
+        cout << "The number is zero.";
+    }
+    else {
+        cout << "The number is not zero.";
+    }
+    return 0;
+}
---
+++ Week 4 rec/rocket_velocity.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double int_velo, final_velo;
+    char fuel;
+    cout << "Enter the initial velocity:" << endl;
+    cin >> int_velo;
+    if(int_velo<0) {
+        cout << "Please input a valid velocity" << endl;
+        return 0;
+    }
+    cout << "Enter the fuel type:" << endl;
+    cin >> fuel;
+    if (int_velo<10) {
+        switch(fuel){
+            case 'A':
+                fuel = 5;
+                break;
+            case 'B':
+                fuel = 10;
+                break;
+            case 'C':
+                fuel = 20;
+                break;
+            default: 
+                cout << "Please input a valid fuel type" << endl;
+                return 0;
+        }
+    } else if (int_velo>=10 && int_velo<=40) {
+        switch(fuel){
+            case 'A':
+                fuel = 6;
+                break;
+            case 'B':
+                fuel = 12;
+                break;
+            case 'C':
+                fuel = 24;
+                break;
+            default: 
+                cout << "Please input a valid fuel type" << endl;
+                return 0;
+        }
+    } else if (int_velo>40) {
+        switch(fuel){
+            case 'A':
+                fuel = 3;
+                break;
+            case 'B':
+                fuel = 6;
+                break;
+            case 'C':
+                fuel = 9;
+                break;
+            default: 
+                cout << "Please input a valid fuel type" << endl;
+                return 0;
+        }
+    } 
+    final_velo = int_velo+(20*fuel);
+    cout << "The final velocity is " << final_velo << " m/s." << endl;
+    return 0;
+}
